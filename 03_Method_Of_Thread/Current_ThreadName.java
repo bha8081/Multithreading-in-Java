1430da22d21c6e6a4bc5153bcfe13cc7dfef9402
bha8081 <bhaveshy2000r@gmail.com>
2024-02-17 09:47:11 +0530

    Check the current working thread Name

---
+++ 03_Method_Of_Thread/Current_ThreadName.java
@@ -0,0 +1,11 @@
+// Check the current working thread Name.
+public class Current_ThreadName
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+
+        // Check current working thread name.
+        System.out.println(Thread.currentThread() .getName());   
+    }
+}
