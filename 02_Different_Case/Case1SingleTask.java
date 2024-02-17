a6d69dc181178d15e9b45351c93a66821fe8a793
bha8081 <bhaveshy2000r@gmail.com>
2024-02-17 07:32:50 +0530

    Case 1 Blc file of multithreading

---
+++ 02_Different_Case/Case1SingleTask.java
@@ -0,0 +1,20 @@
+//1. Create Single task from single therad 
+public class Case1SingleTask extends Thread
+{
+	public void run()
+	{
+		System.out.println("This is the Single task thread: ");
+
+	}
+
+}
+
+// public class Case1SingleTask
+// {
+// 	public static void main(String[] args)
+// 	{
+// 		SingleTask st = new SingleTask();
+
+// 		st.start();
+// 	}
+// }
