9e23059c5ed00470a59f29f09adb3b535e5698d3
bha8081 <bhaveshy2000r@gmail.com>
2024-03-21 10:11:36 +0530

    Interrupted method in java

--- 09_Interrupt_Method/Interrupt_Demo.java
+++ 09_Interrupt_Method/Interrupt_Demo.java
@@ -2,6 +2,8 @@ public class Interrupt_Demo extends Thread
 {
     public void run()
     {
+        System.out.println(Thread.interrupted());
+        System.out.println(Thread.currentThread() .isInterrupted());
         try {
             for(int i = 0; i <= 8; i++)
             {
